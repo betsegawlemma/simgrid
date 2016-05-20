betsegawlemma/simgrid

commit 772f8dd3b48f0c9dff20f67a4c2ea1cbbe673f8d

[SMPI] Add caller location to tracefiles
Please note that we can't use stack unwinding because that
would make the simulation less faithful, as it introduces
non-neglegible overhead.

This implementation is still subject to change.

src/instr/instr_trace.cpp
@@ -5,6 +5,7 @@
  * under the terms of the license (GNU LGPL) which comes with this package. */
 
 #include "src/instr/instr_private.h"
+#include "src/instr/instr_smpi.h"
 #include "xbt/virtu.h" /* sg_cmdline */
 
 XBT_LOG_NEW_DEFAULT_SUBCATEGORY(instr_trace, instr, "tracing event system");
@@ -341,6 +342,11 @@ void new_pajeSetState (double timestamp, container_t container, type_t type, val
   ((setState_t)(event->data))->container = container;
   ((setState_t)(event->data))->value = value;
 
+  smpi_trace_call_location_t* loc = smpi_trace_get_call_location();
+ 
+  ((setState_t)(event->data))->filename   = loc->filename;
+  ((setState_t)(event->data))->linenumber = loc->linenumber;
+
   XBT_DEBUG("%s: event_type=%d, timestamp=%f", __FUNCTION__, (int)event->event_type, event->timestamp);
 
   insert_into_buffer (event);
@@ -359,6 +365,10 @@ void new_pajePushStateWithExtra (double timestamp, container_t container, type_t
   ((pushState_t)(event->data))->container = container;
   ((pushState_t)(event->data))->value = value;
   ((pushState_t)(event->data))->extra = extra;
+
+  smpi_trace_call_location_t* loc = smpi_trace_get_call_location();
+  ((pushState_t)(event->data))->filename   = loc->filename;
+  ((pushState_t)(event->data))->linenumber = loc->linenumber;
 
   XBT_DEBUG("%s: event_type=%d, timestamp=%f", __FUNCTION__, (int)event->event_type, event->timestamp);
 

include/smpi/mpi.h
@@ -25,4 +25,9 @@
 #define assert(x) MC_assert(x)
 #endif
 
+#if TRACE_CALL_LOCATION
+#include "src/instr/instr_smpi.h"
+#include "smpi_extended_traces.h"
+#endif
+
 #endif

include/smpi/smpi.h
@@ -14,6 +14,7 @@
 #include <xbt/misc.h>
 #include <xbt/function_types.h>
 
+#include "src/instr/instr_smpi.h"
 
 #ifdef _WIN32
 #define MPI_CALL(type,name,args) \
@@ -811,6 +812,12 @@ XBT_PUBLIC(unsigned long long) smpi_rastro_timestamp (void);
 XBT_PUBLIC(void) smpi_sample_1(int global, const char *file, int line, int iters, double threshold);
 XBT_PUBLIC(int) smpi_sample_2(int global, const char *file, int line);
 XBT_PUBLIC(void) smpi_sample_3(int global, const char *file, int line);
+XBT_PUBLIC(void) smpi_trace_set_call_location(const char *file, int line);
+/** Fortran binding **/
+XBT_PUBLIC(void) smpi_trace_set_call_location_(const char *file, int* line);
+/** Fortran binding + -fsecond-underscore **/
+XBT_PUBLIC(void) smpi_trace_set_call_location__(const char *file, int* line);
+XBT_PUBLIC(smpi_trace_call_location_t*) smpi_process_get_call_location(void);
 
 #define SMPI_SAMPLE_LOCAL(iters,thres) for(smpi_sample_1(0, __FILE__, __LINE__, iters, thres); \
                                            smpi_sample_2(0, __FILE__, __LINE__);      \
@@ -908,6 +915,7 @@ if(!name) {                                         \
 }
 
 #define SMPI_VARGET_STATIC(name) name[smpi_process_index()]
+
 
 SG_END_DECL()
 #endif

src/instr/instr_paje_header.cpp
@@ -163,6 +163,10 @@ static void TRACE_header_PajePushState (int basic, int size)
   fprintf(tracing_file, "%%       Container string\n");
   fprintf(tracing_file, "%%       Value string\n");
   if (size) fprintf(tracing_file, "%%       Size int\n");
+  if (false) {
+    fprintf(tracing_file, "%%       Filename string\n");
+    fprintf(tracing_file, "%%       Linenumber int\n");
+  }
   fprintf(tracing_file, "%%EndEventDef\n");
 }
 

src/instr/instr_paje_trace.cpp
@@ -222,6 +222,12 @@ void print_pajeSetState(paje_event_t event) {
 
   print_default_pajeState_row<setState_t>(event);
   stream << " " << static_cast<setState_t>(event->data)->value->id;
+
+  if (true) { // If (sg_config("tracing/caller_location"))
+    stream << " " << static_cast<setState_t>(event->data)->filename
+           << " " << static_cast<setState_t>(event->data)->linenumber;
+  }
+
   print_row();
 }
 
@@ -239,6 +245,12 @@ void print_pajePushState(paje_event_t event) {
       stream << 0;
     }
   }
+
+  if (true) { // If (sg_config("tracing/caller_location"))
+    stream << " " << static_cast<pushState_t>(event->data)->filename
+           << " " << static_cast<pushState_t>(event->data)->linenumber;
+  }
+
   print_row();
 
   if (static_cast<pushState_t>(event->data)->extra != NULL) {

src/instr/instr_private.h
@@ -175,6 +175,8 @@ typedef struct s_setState {
   container_t container;
   type_t type;
   val_t value;
+  const char* filename;
+  int linenumber;
 }s_setState_t;
 
 typedef struct s_pushState *pushState_t;
@@ -183,6 +185,8 @@ typedef struct s_pushState {
   type_t type;
   val_t value;
   int size;
+  const char* filename;
+  int linenumber;
   void* extra;
 }s_pushState_t;
 

src/instr/instr_smpi.h
@@ -1,0 +1,20 @@
+#ifndef INSTR_SMPI_H_
+#define INSTR_SMPI_H_ 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef struct smpi_trace_call_location {
+  const char* filename;
+  int linenumber;
+
+  const char* previous_filename;
+  int previous_linenumber;
+} smpi_trace_call_location_t;
+
+smpi_trace_call_location_t* smpi_trace_get_call_location();
+
+#ifdef __cplusplus
+}
+#endif
+#endif

commit 11c2e2af7ba112e62d45f36b8316837442e70bd2

[SMPI] Fix segfaults introduced by 772f8dd3b48f0c9dff20f67a4c2ea1cbbe673f8d
This changeset fixes tests such as msg-trace-masterworker.

src/instr/instr_trace.cpp
@@ -342,10 +342,11 @@ void new_pajeSetState (double timestamp, container_t container, type_t type, val
   ((setState_t)(event->data))->container = container;
   ((setState_t)(event->data))->value = value;
 
-  smpi_trace_call_location_t* loc = smpi_trace_get_call_location();
- 
-  ((setState_t)(event->data))->filename   = loc->filename;
-  ((setState_t)(event->data))->linenumber = loc->linenumber;
+  if (xbt_cfg_get_boolean("smpi/trace-call-location")) {
+    smpi_trace_call_location_t* loc = smpi_trace_get_call_location();
+    ((setState_t)(event->data))->filename   = loc->filename;
+    ((setState_t)(event->data))->linenumber = loc->linenumber;
+  }
 
   XBT_DEBUG("%s: event_type=%d, timestamp=%f", __FUNCTION__, (int)event->event_type, event->timestamp);
 
@@ -366,9 +367,11 @@ void new_pajePushStateWithExtra (double timestamp, container_t container, type_t
   ((pushState_t)(event->data))->value = value;
   ((pushState_t)(event->data))->extra = extra;
 
-  smpi_trace_call_location_t* loc = smpi_trace_get_call_location();
-  ((pushState_t)(event->data))->filename   = loc->filename;
-  ((pushState_t)(event->data))->linenumber = loc->linenumber;
+  if (xbt_cfg_get_boolean("smpi/trace-call-location")) {
+    smpi_trace_call_location_t* loc = smpi_trace_get_call_location();
+    ((pushState_t)(event->data))->filename   = loc->filename;
+    ((pushState_t)(event->data))->linenumber = loc->linenumber;
+  }
 
   XBT_DEBUG("%s: event_type=%d, timestamp=%f", __FUNCTION__, (int)event->event_type, event->timestamp);
 