85a482427a7d15dc34870349b77081b9fbfdcdf1
Martin Quinson <martin.quinson@loria.fr>
2017-06-11 21:49:26 +0200

    further simplify this example, now with examples in tesh

--- examples/msg/energy-link/energy-link.cpp
+++ examples/msg/energy-link/energy-link.cpp
@@ -25,60 +25,52 @@ void sender(std::vector<std::string> args) {
    /* - Send the task to the @ref worker */
    char* payload = bprintf("%f", comm_size);
 
-   XBT_INFO("traffic %f", comm_size);
-
    simgrid::s4u::this_actor::send(mailbox, payload, comm_size);
-
    XBT_INFO("sender done %f",MSG_get_clock());
-
-   simgrid::s4u::this_actor::send(mailbox, xbt_strdup("finalize"), 0);
 }
 
 void receiver() {
    XBT_INFO("Receiving ...");
 
-   while (1) {
+   simgrid::s4u::MailboxPtr mailbox = simgrid::s4u::Mailbox::byName(std::string("message"));
 
-      simgrid::s4u::MailboxPtr mailbox = simgrid::s4u::Mailbox::byName(std::string("message"));
+   char* res = static_cast<char*>(simgrid::s4u::this_actor::recv(mailbox));
+   xbt_assert(res != nullptr, "Some problem");
 
-      char* res = static_cast<char*>(simgrid::s4u::this_actor::recv(
-          mailbox));
-      xbt_assert(res != nullptr, "Some problem");
-
-      if (strcmp(res, "finalize") == 0) { /* - Exit if 'finalize' is received */
-        xbt_free(res);
-        break;
-      }
-      xbt_free(res);
-    }
-    XBT_INFO("receiver done %f",MSG_get_clock());
-
+   xbt_free(res);
+   XBT_INFO("receiver done %f",MSG_get_clock());
 }
 
 int main(int argc, char* argv[]) {
-  if (strcmp(argv[argc-1], "--NS3") == 0) {
+   
+  /* Check if we got --NS3 on the command line, and activate ecofen if so */
+  bool NS3 = false;
+  for (int i = 0; i<argc; i++) {
+     if (strcmp(argv[i], "--NS3") == 0) {
+	NS3 = true;
+     }
+     if (NS3)  // Found the --NS3 parameter; shift the rest of the line
+       argv[i] = argv[i+1];	  
+  }
+  if (NS3) {
     XBT_INFO("Activating the Ecofen energy plugin");
     ns3_link_energy_plugin_init();
-    argv[argc-1] = nullptr;
-    argc--;
+    argc -= 1; // We removed it from the parameters
   } else {
     XBT_INFO("Activating the SimGrid link energy plugin");
     sg_link_energy_plugin_init();
   }
 
+  /* Now initialize the S4U engine, and load the platform */
   simgrid::s4u::Engine* e = new simgrid::s4u::Engine(&argc, argv);
   xbt_assert(argc > 1, "\nUsage: %s platform_file [datasize] [--NS3]\n"
       "\tExample: %s s4uplatform.xml \n"
       "\tIf you add NS3 as last parameter, this module will try to activate the ecofen plugin.\n"
       "\tWithout it, it will use the SimGrid link energy plugin.\n", argv[0],
       argv[0]);
-
-  e->loadPlatform(argv[1]); /** - Load the platform description */
+  e->loadPlatform(argv[1]);
   
-  XBT_INFO("argc: %d", argc);
-  for (int i = 0; i<argc; i++)
-     XBT_INFO("argv[%d]=%s", i, argv[i]);
-   
+  /* prepare to launch the actors */
   std::vector<std::string> args;
   if (argc > 2) 
      args.push_back(argv[2]); // Take the datasize from the command line
@@ -88,7 +80,8 @@ int main(int argc, char* argv[]) {
   simgrid::s4u::Actor::createActor("sender", simgrid::s4u::Host::by_name("SRC"), sender, args);
   simgrid::s4u::Actor::createActor("receiver", simgrid::s4u::Host::by_name("DST"), receiver);
    
-  e->run(); /** - Run the simulation */
+  /* And now, launch the simulation */
+  e->run();
 
   return 0;
 }
