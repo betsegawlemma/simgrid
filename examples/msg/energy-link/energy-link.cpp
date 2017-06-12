e7c90c99bd7ad5705bdd9e1dbbd9becbad59fd77
Martin Quinson <martin.quinson@loria.fr>
2017-06-12 18:23:12 +0200

    make it possible to test multiple flows with this example

--- examples/msg/energy-link/energy-link.cpp
+++ examples/msg/energy-link/energy-link.cpp
@@ -11,33 +11,69 @@
 #include "simgrid/plugins/link_energy.h"
 #include "simgrid/msg.h"
 
+#include <random>
+
+/* Parameters of the random generation of the flow size */
+static const unsigned long int min_size = 1e6;
+static const unsigned long int max_size = 1e9;
+
 XBT_LOG_NEW_DEFAULT_CATEGORY(s4u_app_energyconsumption,
     "Messages specific for this s4u example");
 
 void sender(std::vector<std::string> args) {
-   xbt_assert(args.size() == 1,
-	      "The master function expects 1 arguments from the XML deployment file");
-   XBT_INFO("Send '%s' bytes", args.at(0).c_str());
-   double comm_size = std::stod(args.at(0));
+   xbt_assert(args.size() == 2, "The master function expects 2 arguments.");
+   int flow_amount = std::stoi(args.at(0));
+   double comm_size = std::stod(args.at(1));
+   XBT_INFO("Send %.0f bytes, in %d flows", comm_size, flow_amount);
 
    simgrid::s4u::MailboxPtr mailbox = simgrid::s4u::Mailbox::byName(std::string("message"));
 
    /* - Send the task to the @ref worker */
    char* payload = bprintf("%f", comm_size);
 
-   simgrid::s4u::this_actor::send(mailbox, payload, comm_size);
+   if (flow_amount == 1) {
+	   simgrid::s4u::this_actor::send(mailbox, payload, comm_size);
+   } else {
+	   // Start all comms in parallel
+	   std::vector<simgrid::s4u::CommPtr> comms;
+	   for (int i=0; i<flow_amount; i++)
+		   comms.push_back(simgrid::s4u::this_actor::isend(mailbox,
+				   	   const_cast<char*>("message"), comm_size));
+
+	   // And now, wait for all comms. Manually since wait_all is not part of this_actor yet
+	   for (int i=0; i<flow_amount; i++) {
+		   simgrid::s4u::CommPtr comm = comms.at(i);
+		   comm->wait();
+	   }
+	   comms.clear();
+   }
    XBT_INFO("sender done %f",MSG_get_clock());
 }
 
-void receiver() {
-   XBT_INFO("Receiving ...");
+void receiver(std::vector<std::string> args) {
+	int flow_amount = std::stoi(args.at(0));
+
+   XBT_INFO("Receiving %d flows ...", flow_amount);
 
    simgrid::s4u::MailboxPtr mailbox = simgrid::s4u::Mailbox::byName(std::string("message"));
 
-   char* res = static_cast<char*>(simgrid::s4u::this_actor::recv(mailbox));
-   xbt_assert(res != nullptr, "Some problem");
+   if (flow_amount == 1) {
+	   char* res = static_cast<char*>(simgrid::s4u::this_actor::recv(mailbox));
+	   xbt_assert(res != nullptr, "Some problem");
+	   xbt_free(res);
+   } else {
+	   void *ignored;
 
-   xbt_free(res);
+	   // Start all comms in parallel
+	   std::vector<simgrid::s4u::CommPtr> comms;
+	   for (int i=0; i<flow_amount; i++)
+		   comms.push_back(simgrid::s4u::this_actor::irecv(mailbox, &ignored));
+
+	   // And now, wait for all comms. Manually since wait_all is not part of this_actor yet
+	   for (int i=0; i<flow_amount; i++)
+		   comms.at(i)->wait();
+	   comms.clear();
+   }
    XBT_INFO("receiver done %f",MSG_get_clock());
 }
 
@@ -65,6 +101,10 @@ int main(int argc, char* argv[]) {
   simgrid::s4u::Engine* e = new simgrid::s4u::Engine(&argc, argv);
   if (NS3)
      xbt_cfg_set_parse("network/model:NS3");
+  else
+	  /* Use the simple, intuitive model of SimGrid, even if it's wrong on slow start */
+	 xbt_cfg_set_parse("network/model:CM02");
+
    
   xbt_assert(argc > 1, "\nUsage: %s platform_file [datasize] [--NS3]\n"
       "\tExample: %s s4uplatform.xml \n"
@@ -74,14 +114,35 @@ int main(int argc, char* argv[]) {
   e->loadPlatform(argv[1]);
   
   /* prepare to launch the actors */
-  std::vector<std::string> args;
-  if (argc > 2) 
-     args.push_back(argv[2]); // Take the datasize from the command line
-  else 
-     args.push_back("1000"); // Use the default datasize
-   
-  simgrid::s4u::Actor::createActor("sender", simgrid::s4u::Host::by_name("SRC"), sender, args);
-  simgrid::s4u::Actor::createActor("receiver", simgrid::s4u::Host::by_name("DST"), receiver);
+  std::vector<std::string> argSender;
+  std::vector<std::string> argReceiver;
+  if (argc > 2) {
+	  argSender.push_back(argv[2]); // Take the amount of flows from the command line
+	  argReceiver.push_back(argv[2]);
+  } else {
+	  argSender.push_back("1"); // Default value
+	  argReceiver.push_back("1");
+  }
+  if (argc > 3) {
+	 if (strcmp(argv[3], "random") == 0) { // We're asked to get a random size
+	  /* Initialize the random number generator */
+	  std::random_device rd;
+	  std::default_random_engine generator(rd());
+
+	  /* Distribution on which to apply the generator */
+	  std::uniform_int_distribution<unsigned long int> distribution(min_size, max_size);
+
+	  char* size = bprintf("%lu", distribution(generator));
+	  argSender.push_back(std::string(size));
+	  xbt_free(size);
+	 } else { // Not "random" ? Then it should be the size to use
+		 argSender.push_back(argv[3]); // Take the datasize from the command line
+	 }
+  } else { // No parameter at all? Then use the default value
+	  argSender.push_back("1000");
+  }
+  simgrid::s4u::Actor::createActor("sender", simgrid::s4u::Host::by_name("SRC"), sender, argSender);
+  simgrid::s4u::Actor::createActor("receiver", simgrid::s4u::Host::by_name("DST"), receiver, argReceiver);
    
   /* And now, launch the simulation */
   e->run();
